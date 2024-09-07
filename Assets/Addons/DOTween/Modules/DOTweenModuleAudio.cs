ae2b20658249f1486873586f8c9783eb56ad259f
ellie <elliehachemjob@gmail.com>
2024-09-07 19:15:06 +0200

    TweenModule

--- Assets/Addons/DOTween/Modules/DOTweenModuleAudio.cs
+++ Assets/Addons/DOTween/Modules/DOTweenModuleAudio.cs
@@ -13,7 +13,35 @@ using UnityEngine.Audio; // Required for AudioMixer
 #pragma warning disable 1591
 namespace DG.Tweening
 {
-	public static class DOTweenModuleAudio
+
+    public static class DOTweenModuleAudio
+    {
+        #region Shortcuts
+
+        #region Audio
+
+        /// <summary>Tweens an AudioSource's volume to the given value.
+        /// Also stores the AudioSource as the tween's target so it can be used for filtered operations</summary>
+        /// <param name="endValue">The end value to reach (0 to 1)</param><param name="duration">The duration of the tween</param>
+        public static TweenerCore<float, float, FloatOptions> DOFade(this AudioSource target, float endValue, float duration)
+        {
+            if (endValue < 0) endValue = 0;
+            else if (endValue > 1) endValue = 1;
+            TweenerCore<float, float, FloatOptions> t = DOTween.To(() => target.volume, x => target.volume = x, endValue, duration);
+            t.SetTarget(target);
+            return t;
+        }
+
+        /// <summary>Tweens an AudioSource's pitch to the given value.
+        /// Also stores the AudioSource as the tween's target so it can be used for filtered operations</summary>
+        /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
+        public static TweenerCore<float, float, FloatOptions> DOPitch(this AudioSource target, float endValue, float duration)
+        {
+            TweenerCore<float, float, FloatOptions> t = DOTween.To(() => target.pitch, x => target.pitch = x, endValue, duration);
+            t.SetTarget(target);
+            return t;
+        }
+	/* public static class DOTweenModuleAudio
     {
         #region Shortcuts
 
@@ -190,13 +218,12 @@ namespace DG.Tweening
         {
             return DOTween.TogglePause(target);
         }
-
         #endregion
 
         #endregion
 #endif
 
         #endregion
-    }
-}
+    }*/
+} 
 #endif
